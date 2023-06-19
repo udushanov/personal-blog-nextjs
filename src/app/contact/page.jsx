505ebe4544b8f1a3b92b891c9d667cb9774acce6
Ulugbek <u.dushanov@gmail.com>
2023-06-19 21:40:02 +0500

    feat: add contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,5 +1,32 @@
-import styles from './page.module.css';
+import Image from "next/image";
+import styles from "./page.module.css";
+import Button from "@/components/button/Button";
 
 export default function Contact() {
-  return <div className={styles.container}>contact</div>;
+  return (
+    <div>
+      <h1 className={styles.title}>Let's keep in touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="/contact.png"
+            alt="background"
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+        <form className={styles.form}>
+          <input type="text" placeholder="name" className={styles.input} />
+          <input type="text" placeholder="email" className={styles.input} />
+          <textarea
+            className={styles.textArea}
+            placeholder="message"
+            cols="30"
+            rows="10"
+          ></textarea>
+          <Button url="#" text="Send" />
+        </form>
+      </div>
+    </div>
+  );
 }
