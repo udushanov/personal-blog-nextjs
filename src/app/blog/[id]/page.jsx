f089b8be9c6143c492e656d1fa6cc49d9440ccd1
Ulugbek <u.dushanov@gmail.com>
2023-06-25 11:33:47 +0500

    feat: add crud operations with mongodb

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 import { notFound } from "next/navigation";
 
 async function getData(id) {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+  const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
     cache: "no-store",
   });
 
@@ -17,35 +17,28 @@ async function getData(id) {
 const BlogPost = async ({ params }) => {
   const data = await getData(params.id);
   return (
-    <div>
+    <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.info}>
           <h1 className={styles.title}>{data.title}</h1>
-          <p className={styles.desc}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
-            ipsum sunt repudiandae labore aliquid eligendi tempore aliquam nemo!
-            Ullam facere eveniet molestiae nesciunt deserunt reiciendis dolorum
-            illum quam quo autem. Maxime illo quidem fugiat in itaque rem
-            quaerat molestias sit?
-          </p>
+          <p className={styles.desc}>{data.desc}</p>
           <div className={styles.author}>
-            <Image alt="img" width={40} height={40} className={styles.avatar} />
-            <span className={styles.username}>John Doe</span>
+            <Image
+              src={data.img}
+              alt=""
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>{data.username}</span>
           </div>
         </div>
         <div className={styles.imageContainer}>
-          <Image alt="img" fill={true} className={styles.image} />
+          <Image src={data.img} alt="" fill={true} className={styles.image} />
         </div>
       </div>
       <div className={styles.content}>
-        <p className={styles.text}>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat nisi
-          fugit quo sapiente libero obcaecati quod consequatur, beatae adipisci
-          ratione itaque natus ducimus molestias. Sed molestiae ipsa, minima
-          reprehenderit atque eveniet iste quos accusantium eum praesentium
-          tenetur illum modi aspernatur, ad veritatis reiciendis nostrum, vero
-          autem unde rem repudiandae aliquid!
-        </p>
+        <p className={styles.text}>{data.content}</p>
       </div>
     </div>
   );
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,9 +1,9 @@
-import Link from "next/link";
 import styles from "./page.module.css";
+import Link from "next/link";
 import Image from "next/image";
 
 async function getData() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+  const res = await fetch("http://localhost:3000/api/posts", {
     cache: "no-store",
   });
 
@@ -14,37 +14,33 @@ async function getData() {
   return res.json();
 }
 
-export default async function Blog() {
+const Blog = async () => {
   const data = await getData();
-
   return (
-    <div>
-      {data.map((item) => {
-        // console.log(item);
+    <div className={styles.mainContainer}>
+      {data.map((item) => (
         <Link
-          href={`/blog/${item.id}`}
+          href={`/blog/${item._id}`}
           className={styles.container}
           key={item.id}
-          // as={"image"}
         >
-          <div>
+          <div className={styles.imageContainer}>
             <Image
-              src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+              src={item.img}
+              alt=""
               width={400}
               height={250}
-              alt="img"
-              // placeholder="blur"
-              // blurDataURL="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-              // priority={true}
               className={styles.image}
             />
           </div>
           <div className={styles.content}>
             <h1 className={styles.title}>{item.title}</h1>
-            <p className={styles.desc}>{item.body}</p>
+            <p className={styles.desc}>{item.desc}</p>
           </div>
-        </Link>;
-      })}
+        </Link>
+      ))}
     </div>
   );
-}
+};
+
+export default Blog;
